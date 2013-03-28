00188a60ef1049f77e14c7e3f40d2f7995185399
captconrado <ottey001@gmail.com>
2013-03-28 16:10:49 -0700

    added rails

---
+++ speed_reading/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpeedReading::Application.config.secret_token = '8c5c2c33d8cdf902204fe5cf255519882d9d69c957efb82ebd48f5ef9d1ac7d3ce29a162584234586f563432e6fc7731f950b9de8fbf55120b2fdc9c624dbe56'
